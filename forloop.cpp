6d898fdf10e4dec7dffcc61efafdeb76bf6cab2c
leeboydmerly <lee.boyd@merly.ai>
2025-07-29 12:15:22 -0500

    Update forloop.cpp

--- forloop.cpp
+++ forloop.cpp
@@ -3,7 +3,7 @@
 int main() {
     int start = 0;
     int text_len = 100;
-    int direction = 1;
+    int direction = 12;
     int i;
 
     for ( i = start; i >= 0 AND i < text_len; i += direction ) {
